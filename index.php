691bd67da650f2447de8b6ea92338836430caf3c
dhara09 <dhara.salot@techsevin.com>
2019-01-17 15:06:53 +0530

    modified main page

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-include('login.php');
-include('signup.php');
+// include('login.php');
+// include('signup.php');
+?>
+<html>
+<center>
+<button onclick="window.location.href='login.php'" type="submit" value="submit" name="submit">Login</button>
+<button onclick="window.location.href='signup.php'" type="submit" value="submit" name="submit">Signup</button>
+</center>
+
+<?php
+include('footer.php');
 ?>
