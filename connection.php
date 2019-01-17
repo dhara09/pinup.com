e78dbdd086c504654a74ca5369b1d84328fcfad8
dhara09 <dhara.salot@techsevin.com>
2019-01-17 15:11:26 +0530

    db connection file

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$host="localhost";
+$user="root";
+$password="123456";
+$dbname="userDatabase";
+$con=mysqli_connect($host,$user,$password,$dbname);
+
+if(mysqli_connect_errno())
+{
+die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
+else
+{
+  echo "Connected succcessfully !! ";
+}
+?>
