c36e85ec7e0dd236d33f5ad9c54d1b950f05f749
dhara09 <dhara.salot@techsevin.com>
2019-01-18 19:05:06 +0530

    javascript validations for signup

--- signup.php
+++ signup.php
@@ -1,92 +1,98 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.js"></script> 
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script type="text/javascript"></script> 
 <script>
 $(document).ready(function()
-{ 
+{	
 	$("input").focus(function()
 	{
     	$(this).next("span").empty();
 	});
-
-		$("#submit").submit(function()
+	$("#submit").click(function()
     {
-				var names=$("#name1").val();
-				var check=1;
-				if ($ ("#name1").val() === "")
-				{
-					$("#namespan").text("Please fill the field").show();
-					check=0;
-				}
-				else if(!names.match('\^[a-zA-Z]*$'))
-				{
-					 $("#namespan").text("Enter only alphabets").show();
-					 check=0;
-				}
+		var names=$("#name1").val();
+		var check=1;
+        if(names=="")
+        {
+			$("#namespan").text("Please fill this field").show();
+			check=0;
+		}
+		
+		else if(!names.match('\^[a-zA-Z]*$'))
+		{
+			$("#namespan").text("Use only alphabets").show();
+			check=0;
+		}
 
-				var lnames=$("#lname1").val();
-				if ($ ("#lname1").val() === "")
-				{
-					$("#lnamespan").text("Please fill this field").show();
-					check=0;
-				}
-				else if(!lnames.match('\^[a-zA-Z]*$'))
-				{
-					$("#lnamespan").text("Enter only alphabets").show();
-					check=0;
-				}
-				var emails=$("#email1").val();
-				if ($ ("#email1").val() === "")
-				{
-					$("#emailspan").text("Please fill this field").show();
-					check=0;
-				} 
-				else if (!emails.match('/^([w-.]+@([w-]+.)+[w-]{2,4})?$/'))
-				{
-					$("#emailspan").text("Enter valid email address").show();
-					check=0;
-				} 
-				var contacts =$("#contact1").val();
-				if ($ ("#contact1").val() === "")
-				{
-					$("#contactspan").text("Please fill this field").show();
-					check=0;
-				}
-				else if(!contacts.match('^[0-9]*$') )
-				{
-					$("#contactspan").text("Enter only Numbers").show();
-					check=0;
-				}
-				else if(contacts.length() > 10)
-				{
-					$("#contactspan").text("Enter only 10 digits Number").show();
-					check=0;
-				}
-				var addr1=$("#address1").val();
-				if ($ ("#address1").val() === "")
-				{
-					$("#addr1span").text("Please fill this field").show();
-					check=0;
-				}
-        
-        var pass=$("#password1").val();
-				if ($ ("#password1").val() === "")
-				{
-					$("#passspan").text("Please fill this field").show();
-					check=0;
-				}
-        
-        var cpass=$("#confrmpass1").val();
-				if ($ ("#confrmpass1").val() === "")
-				{
-					$("#cpaspan").text("Please fill this field").show();
-					check=0;
-				}
+		var lnames=$("#lname1").val();
+		if(lnames == "")
+		{
+			$("#lnamespan").text("please fill this field").show();
+			$check=0;
+		}
 
-        if($pass !== $cpass){
-          $("#cpaspan").text("password dont match").show();
+		else if(!lnames.match('\^[a-zA-Z]*$'))
+		{
+			$("#lnamespan").text("Use only alphabets").show();
+			check=0;
+		}
+
+		var email=$("#email1").val();
+		if(email == "")
+		{
+			$("#emailspan").text("please fill this field").show();
+			$check=0;
+		}
+		/* else if(!email.match('/^([w-.]+@([w-]+.)+[w-]{2,4})?$/'))
+		{
+			$("#emailspan").text("Enter valid email address").show();
+			$check=0;
+		} */
+		
+		var address=$("#address1").val();
+		if(address == "")
+		{
+			$("#addr1span").text("please fill this field").show();
+			$check=0;
+		}
+		
+		var contact=$("#contact1").val();
+		if(contact == "")
+		{
+			$("#contact1span").text("please fill this field").show();
+			$check=0;
+		}
+		else if(!contact.match('^[0-9]*$'))
+		{
+			$("#contact1span").text("Use only numbers").show();
+			$check=0;	
+		}
+		 /* else if(contact.length() >10)
+		{
+			$("#contact1span").text("Only 10 digit Number").show();
+			$check=0;
+
+		}  */
+		var pass=$("#password1").val();
+		if(pass == "")
+		{
+			$("#passspan").text("please fill this field").show();
+			$check=0;
+		}
+		var cpass=$("#confrmpass1").val();
+		if(cpass== "")
+		{
+			$("#cpaspan").text("please fill this field").show();
+			$check=0;
+		}
+		if(check==1)
+		{ 
+			alert("success");
+            window.location.href='signupSave.php';
         }
-				return false;
-				if(check==1)
-				{ alert("form submitted"); }
-		});
+        return false;
+	});
 });
 </script>
 <style>
@@ -94,23 +100,19 @@ $(document).ready(function()
 {
   color:red;
 }
-</style>
-<?php
-include('validation.php');
-?>
-        <form name="form"  action="" method="POST">
+</style> 
           <label for="name"><b>Name</b></label>
-          <br><input id="name1" type="text" placeholder="Enter Name" name="Users[name]" ></br>
+          <br><input id="name1" type="text" placeholder="Enter Name" name="Users[name]"  ></br>
           <span class='error'></span>
 	      	<span id="namespan" class="error"></span>
 
           <label for="lastname"><b>Last Name</b></label>
-          <br> <input id="lname1" type="text" placeholder="Enter LastName" name="Users[lastname]" ></br>
+          <br> <input id="lname1" type="text" placeholder="Enter LastName" name="Users[lastname]" value="salot" ></br>
           <span class='error'></span>
 		      <span id="lnamespan" class="error"></span>
 
           <label for="email"><b>Email</b></label>
-          <br> <input id="email1" type="text" placeholder="Enter Email" name="Users[email]" ></br>
+          <br> <input id="email1" type="text" placeholder="Enter Email" name="Users[email]" value="abc@gmail.com"></br>
           <span class='error'></span>
 	      	<span id="emailspan" class="error"></span>
 
@@ -134,9 +136,9 @@ include('validation.php');
           <span class='error'></span>
 		      <span id="cpaspan" class="error"></span>
 
-          <button type="submit" value="submit" name="submit" class="signupbtn" style="align:justify">Register</button>
+          <button type="submit" value="submit" id="submit" name="submit" class="signupbtn" style="align:justify">Register</button>
           <?php 
           // if(isset($_GET['errmsg']))echo $_GET['errmsg']; 
              if($errmsg!= "")echo $errmsg; 
             ?> 
-          </form>
+         
