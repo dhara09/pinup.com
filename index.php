17010f35783a19d58e61cb70cbfe2c795c6ce9e4
dhara09 <dhara.salot@techsevin.com>
2019-01-18 18:07:34 +0530

    main file

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <button onclick="window.location.href='login.php'" type="submit" value="submit" name="submit">Login</button>
 <button onclick="window.location.href='signup.php'" type="submit" value="submit" name="submit">Signup</button>
 </center>
-
 <?php
 include('footer.php');
 ?>
