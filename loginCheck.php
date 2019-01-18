8cefe64e66926fd16e21e964f1e6809678947bed
dhara09 <dhara.salot@techsevin.com>
2019-01-18 18:02:19 +0530

    php validations insertion and connection for db for login

--- loginCheck.php
+++ loginCheck.php
@@ -1,23 +1,43 @@
 <?php
-//php validations and inserting data
-
-if(isset($_POST['submit']))
+//echo "test";
+//include('welcome.php'); 
+if(isset($_POST['button']))
 {
     $name=$_POST['Users']['name'];
     $pass=$_POST['Users']['password'];
-    
-    if(empty($name)){
+    $errmsg='';
+    $check=0;
+
+    if(empty($name))
+    {
        $errmsg .= "Fill Name";
        $check=1;
     }
-    if(empty($pass)){
+    if(empty($pass))
+    {
         $errmsg .= "Fill password";
         $check=1;
     }
-    if ($check==0){
-       $errmsg .="logged in successfully";
+    if ($check==0)
+    {
+        $errmsg .="logged in successfully";
     }
-    //echo "$errmsg";
-    //header("Location:http://local.pinup.com/login.php?error=$errmsg");
 }
+include('connection.php');
+$email=$_POST['Users']['email'];
+$pass=$_POST['Users']['password'];
+ $query = mysqli_query("SELECT email ,password FROM User WHERE email=$email AND password=$pass", $con);
+
+  if (mysqli_num_rows($query) == 0)
+  {
+      //echo "welcome ";
+      header("Location:welcome.php");
+    
+  }
+ else
+  {
+    echo "Try again";
+    header("Location:login.php");
+  }  
+
 ?>
