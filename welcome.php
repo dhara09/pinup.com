d50c6cf2802db45e9d2dc55133ff0d545b28831e
dhara09 <dhara.salot@techsevin.com>
2019-01-21 16:11:02 +0530

    welcome page using session

--- welcome.php
+++ welcome.php
@@ -1,4 +1,20 @@
 <?php
 session_start();
-echo"welcome user";
+require('loginCheck.php');
+//echo"welcome user";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h4> Welcome <?php echo $_SESSION['name'];?></h4>
+    <a href="logout.php"> Logout</a>
+</body>
+</html>
