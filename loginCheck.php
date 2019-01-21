5e989104c4069d3e3c89d04e1f95d024e1b4c487
dhara09 <dhara.salot@techsevin.com>
2019-01-21 16:09:53 +0530

    php validations for login with sql query

--- loginCheck.php
+++ loginCheck.php
@@ -1,6 +1,5 @@
 <?php
-//echo "test";
-//include('welcome.php'); 
+//include('login.php');
 if(isset($_POST['button']))
 {
     $name=$_POST['Users']['name'];
@@ -20,24 +19,145 @@ if(isset($_POST['button']))
     }
     if ($check==0)
     {
-        $errmsg .="logged in successfully";
+        echo "done";
+        //$errmsg .="logged in successfully";
+        //echo "<script>alert('logged in'); window.location = 'welcome.php'</script>";
+
+        //include("welcome.php");
+       // header("location:welcome.php");
     }
+    header("Location:http://local.pinup.com/login.php?error=$errmsg");
+    //header("Location:welcome.php");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------- */
 include('connection.php');
-$email=$_POST['Users']['email'];
+function Insertdata($table,$field_values,$data_values)
+    {
+        $field_values= implode(',',$field);
+        $data_values=implode(',',$data);
+
+        $sql="INSERT into". " ".$table." ".$field_values. "VALUES(".$data_values.")";
+        $result=$conn->query($sql);
+    }
+    $query = Insertdata('User',$_POST['Users']);
+
+    //$query = sqlInsert('student',$_POST['student']);
+    $query1=Insertdata('userDetail',$_POST['UserDetails']); 
+    mysqli_close($conn);
+/* session_start();
+require_once('connection.php');
+//echo"logged in";
+exit; */
+
+/* ------------------------------------------- */
+//header("location:welcome.php");
+/* $name=$_POST['Users']['name'];
 $pass=$_POST['Users']['password'];
- $query = mysqli_query("SELECT email ,password FROM User WHERE email=$email AND password=$pass", $con);
+$encrypt=md5($pass);
 
-  if (mysqli_num_rows($query) == 0)
-  {
-      //echo "welcome ";
-      header("Location:welcome.php");
-    
-  }
- else
-  {
-    echo "Try again";
-    header("Location:login.php");
-  }  
+$query = mysqli_query("SELECT * FROM User WHERE name=$name AND password=$encrypt", $con);
+$result=mysqli_query($query);
+$count=mysqli_num_rows($result);
 
+if($count==1){
+    echo "welcome";
+    session_start();
+    $_SESSION['name'] =$name;
+    $_SESSION['password'] =$pass;
+    header("Locaton:welcome.php");
+}
+else{
+    echo "try again";
+    //header("Location:welcome.php");
+} */
+  /* if (!mysqli_num_rows($query) == 0)
+  {
+    echo "welcome ";
+    session_start();
+    $_SESSION['name'] =$name;
+    $_SESSION['password'] =$encrypt;
+    header("Location:welcome.php");
+  } */
+ 
+/* -------------------- */
+ /*  $email = $_POST['Users']['email'];
+  $password = $_POST['Users']['password'];
+
+  $cekuser = mysqli_query("SELECT * FROM User WHERE email = '$email' AND password ='$password' ");
+  $row = mysqli_num_rows($cekuser);
+  $c= mysqli_fetch_array($cekuser); 
+  if($row == 0) 
+  {
+      echo "<script>alert('enter email and pass!'); window.location = 'login.php'</script>";
+  } 
+  else 
+  {
+      if($pass > $c['Users']['password']) 
+      {
+        echo "<script>alert('Wrong password!'); window.location = 'login.php'</script>";
+      } 
+      else 
+      {
+        $_SESSION['email'] = $c['Users']['email'];
+        header('location:login.php');
+      }
+  } */
+/* -------------- */
+/* $name=$_POST['Users']['name'];
+$pass=$_POST['Users']['password'];
+$encrypt=md5($pass);
+
+$query = mysqli_query("SELECT name,password FROM User WHERE name=$name AND password=$encrypt", $con);
+$result=mysqli_query($query);
+$count=mysqli_num_rows($result);
+
+if($count==1){
+    session_start();
+    $_SESSION['name'] =$name;
+    $_SESSION['password'] =$encrypt;
+    header("Locaton:welcome.php");
+}
+else{
+    echo "try again";
+    //header("Location:login.php");
+}   */
 ?>
+
+<!-- $name=$_POST['Users']['name'];
+    $pass=$_POST['Users']['password'];
+    $encrypt=md5($pass);
+    $query = mysqli_query("SELECT name,password FROM User WHERE name=$name AND password=$encrypt", $con);
+    $result=mysqli_query($query);
+
+    $count=mysqli_num_rows($result);
+    if($count==1)
+    //if (mysqli_num_rows($query) == 1)
+    {
+        $_SESSION['name']=$_POST['Users']['name'];
+        $_SESSION['password']=$_POST['Users']['password'];
+
+header("location:welcome.php");
+}
+else {
+    echo "try again";
+//header("location:login.php");
+ -->}
