6a4a4d37041b0c53fd7310a479529911db248ec3
dhara09 <dhara.salot@techsevin.com>
2019-01-31 19:35:11 +0530

    header parameters

--- loginCheck.php
+++ loginCheck.php
@@ -1,56 +1,55 @@
 <?php
-if(isset($_POST['button'])) 
+if(isset($_POST['Users']['email'])) 
 {
-    $test="hello";
-    $name=$_POST['Users']['name'];
-    $password=$_POST['Users']['password'];
     $errmsg='';
     $check=0;
+    $query=array(
+        $email=$_POST['Users']['email'],
+        $password=$_POST['Users']['password'],
+    ); 
+    //$query= http_build_query($query);  
+   // $arg = base64_encode( json_encode($query) );
 
-    if(empty($name))
+    $email=$_POST['Users']['email'];
+    $a=urlencode($email);
+    $password=$_POST['Users']['password'];
+    $b=urlencode($password);
+    if(empty($email))
     {
-       $errmsg .= "Fill name </br>";
+       $errmsg .= "Fill email </br>";
        $check=1;
+    }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        $errmsg .=" Enter valid Email Address <br>";
+        $check=1;
     }
     if(empty($password))
     {
         $errmsg .= "Fill password";
         $check=1;
     }
-    if($check!=0)
+    if($check == 1)
     {
-
-        header("Location:http://local.pinup.com/login.php?error=$errmsg&user=$name");
-    }
-    else
-    {
-        header("location:loginCheck.php");
+        $url="http://local.pinup.com/login.php?error=$errmsg";
+        header("Location:$url");
     }
 }
 require_once("connection.php");
+session_start();
 echo " <br> Checking For existing users from db is in process....";
+$query="SELECT * FROM User WHERE name='$name' and password='".md5($password)."'";
+$result=mysqli_query($con,$query) or die(mysqli_error());
+$rows=mysqli_num_rows($result);
+if($rows == 1)
+{
+    //$_SESSION['name'] = $name;
+    header("Location: welcome.php");
+}
+else
+{
+   echo "combination of name & pass dont match.<p>Click here to <a href='login.php'>Login</a></div></p>";
+}  
 
-
-
-
-
-// if($_SERVER('REQUEST_METHOD')=='POST')
-/* 
-     session_start();
-    require_once("connection.php");
-    $name=$_POST['Users']['name'];
-    $password=$_POST['Users']['password'];
-    $query="SELECT * FROM User WHERE name='$name' and password='".md5($password)."'";
-	$result=mysqli_query($con,$query) or die(mysqli_error());
-    $rows=mysqli_num_rows($result);
-    if($rows == 1)
-    {
-        $_SESSION['name'] = $name;
-        header("Location: welcome.php");
-    }
-    //else if($rows!=1)
-    else
-    {
-       echo "combination of name & pass dont match.<p>Click here to <a href='login.php'>Login</a></div></p>";
-       $errmsg .="combination dnt match";
-    }   */
+/* $a=urlencode($email);
+    $b=urlencode($password); */
