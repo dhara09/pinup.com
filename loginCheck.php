fa44ed8a4d36f8b08f8126f3f6965a031058d2e9
dhara09 <dhara.salot@techsevin.com>
2019-02-01 18:31:23 +0530

    url enconde and sanitize

--- loginCheck.php
+++ loginCheck.php
@@ -1,55 +1,54 @@
 <?php
+if(!isset($_SESSION)){ 
+    session_start();}
 if(isset($_POST['Users']['email'])) 
-{
-    $errmsg='';
-    $check=0;
-    $query=array(
-        $email=$_POST['Users']['email'],
-        $password=$_POST['Users']['password'],
-    ); 
-    //$query= http_build_query($query);  
-   // $arg = base64_encode( json_encode($query) );
+{ 
+        $errmsg='';
+        $check=0;
+        $query=array(
+            $email=$_POST['Users']['email'],
+            $password=$_POST['Users']['password'],
+        );  
+        $_SESSION['query']=$query;
+       /*  echo "<pre>";
+        print_r($_SESSION);
+        echo "</pre>";
+        exit;  */
+        //$query=http_build_query($query);  
+        //$_SESSION['query']=$query;
 
-    $email=$_POST['Users']['email'];
-    $a=urlencode($email);
-    $password=$_POST['Users']['password'];
-    $b=urlencode($password);
-    if(empty($email))
-    {
-       $errmsg .= "Fill email </br>";
-       $check=1;
-    }
-    else if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-    {
-        $errmsg .=" Enter valid Email Address <br>";
-        $check=1;
-    }
-    if(empty($password))
-    {
-        $errmsg .= "Fill password";
-        $check=1;
-    }
-    if($check == 1)
-    {
-        $url="http://local.pinup.com/login.php?error=$errmsg";
-        header("Location:$url");
-    }
+        //$email=$_POST['Users']['email'];
+        //$password=$_POST['Users']['password'];
+        if(empty($email))
+        {
+            $errmsg .= "Fill email </br>" ;
+            $check=1;
+        }
+        else if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            $errmsg .= "Enter valid Email Address <br>" ;
+            $check=1;
+        }
+        if(empty($password))
+        {
+            $errmsg .= "Fill password" ;
+            $check=1;
+        }
+        if($check == 1)
+        {
+            //header("Location:http://local.pinup.com/login.php?error=".urlencode(serialize($query)));
+            header("Location:http://local.pinup.com/login.php?error=$errmsg");
+        }
+        $_SESSION['email']=$_POST['Users']['email'];
 }
 require_once("connection.php");
-session_start();
 echo " <br> Checking For existing users from db is in process....";
-$query="SELECT * FROM User WHERE name='$name' and password='".md5($password)."'";
+$query="SELECT * FROM User WHERE name='$name' and password='".SHA($password)."'";
 $result=mysqli_query($con,$query) or die(mysqli_error());
 $rows=mysqli_num_rows($result);
-if($rows == 1)
-{
-    //$_SESSION['name'] = $name;
+if($rows == 1){
     header("Location: welcome.php");
 }
-else
-{
+else{
    echo "combination of name & pass dont match.<p>Click here to <a href='login.php'>Login</a></div></p>";
 }  
-
-/* $a=urlencode($email);
-    $b=urlencode($password); */
