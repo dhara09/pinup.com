399f82c5d850f7a1ac0abee9592d9d1a9b0e907a
dhara09 <dhara.salot@techsevin.com>
2019-02-04 19:15:35 +0530

    condition with proper message

--- ajax.php
+++ ajax.php
@@ -1,17 +1,19 @@
 <?php
 //require_once("connection.php");
 require_once("DBController.php");
+
 $db_handle = new DBController();
-if(!empty($_POST["email"])) {
-  $query = "SELECT * FROM users WHERE email='" . $_POST['Users']['email'] . "'";
+if(!empty($_POST['email'])) { 
+  $query = "SELECT email FROM User WHERE email='" . $_POST['email'] . "'";
   $user_count = $db_handle->numRows($query);
-  if($user_count > 0)
+  //  echo $query;
+ if($user_count > 0)
   {
-      echo "<span class='status-available'> Email Available.</span>";
+    echo "<span class='status-not-available'> Email Not Available.</span>";
   }
   else
   {
-      echo "<span class='status-not-available'> Email Not Available.</span>";
+    echo "<span class='status-available'> Email Available.</span>";
   }
 }
 ?>
