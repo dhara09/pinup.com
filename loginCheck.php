f1fd987225bf4f94a56f3a7a44550c26c172bc04
dhara09 <dhara.salot@techsevin.com>
2019-02-04 18:29:11 +0530

    encode url not wrking properly

--- loginCheck.php
+++ loginCheck.php
@@ -1,24 +1,18 @@
 <?php
 if(!isset($_SESSION)){ 
-    session_start();}
+    session_start();
+}
+
+$data=array(
+    $email=$_POST['Users']['email'],
+    $password=$_POST['Users']['password'],
+);   
+//$query= http_build_query(array('em' => $data));
+$query=http_build_query($data);
+$errmsg='';
+$check=0;
 if(isset($_POST['Users']['email'])) 
 { 
-        $errmsg='';
-        $check=0;
-        $query=array(
-            $email=$_POST['Users']['email'],
-            $password=$_POST['Users']['password'],
-        );  
-        $_SESSION['query']=$query;
-       /*  echo "<pre>";
-        print_r($_SESSION);
-        echo "</pre>";
-        exit;  */
-        //$query=http_build_query($query);  
-        //$_SESSION['query']=$query;
-
-        //$email=$_POST['Users']['email'];
-        //$password=$_POST['Users']['password'];
         if(empty($email))
         {
             $errmsg .= "Fill email </br>" ;
@@ -35,15 +29,17 @@ if(isset($_POST['Users']['email']))
             $check=1;
         }
         if($check == 1)
-        {
+        {   
+            $url="http://local.pinup.com/login.php?que=$query";
+            $encode=rawurlencode($url);
+            header("Location:http://local.pinup.co/login.php?.$encode");
             //header("Location:http://local.pinup.com/login.php?error=".urlencode(serialize($query)));
-            header("Location:http://local.pinup.com/login.php?error=$errmsg");
+            //header("Location:http://local.pinup.com/login.php?error=$errmsg&em=$email&pass=$password");
         }
-        $_SESSION['email']=$_POST['Users']['email'];
 }
 require_once("connection.php");
 echo " <br> Checking For existing users from db is in process....";
-$query="SELECT * FROM User WHERE name='$name' and password='".SHA($password)."'";
+$query="SELECT * FROM User WHERE email='$email' and password='".MD5($password)."'";
 $result=mysqli_query($con,$query) or die(mysqli_error());
 $rows=mysqli_num_rows($result);
 if($rows == 1){
