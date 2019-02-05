4e191ab35a2cc38eabe460e9592570372ff66378
dhara09 <dhara.salot@techsevin.com>
2019-02-05 19:05:52 +0530

    check fr messages

--- ajax.php
+++ ajax.php
@@ -1,19 +1,13 @@
 <?php
-//require_once("connection.php");
 require_once("DBController.php");
-
 $db_handle = new DBController();
-if(!empty($_POST['email'])) { 
+if(!empty($_POST['email'])) 
+{ 
   $query = "SELECT email FROM User WHERE email='" . $_POST['email'] . "'";
   $user_count = $db_handle->numRows($query);
-  //  echo $query;
- if($user_count > 0)
-  {
-    echo "<span class='status-not-available'> Email Not Available.</span>";
-  }
-  else
-  {
-    echo "<span class='status-available'> Email Available.</span>";
-  }
+  if($user_count > 0){
+    echo "<span class='status-not-available'> Email Not Available.</span>";}
+  else{
+    echo "<span class='status-available'> Email Available.</span>";}
 }
 ?>
