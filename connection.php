41a37893db03da56a38087316e1bef391553dca4
dhara09 <dhara.salot@techsevin.com>
2019-02-05 19:08:50 +0530

    removed echo

--- connection.php
+++ connection.php
@@ -4,13 +4,7 @@ $user="root";
 $password="123456";
 $dbname="userDatabase";
 $con=mysqli_connect($host,$user,$password,$dbname);
-
-if(mysqli_connect_errno())
-{
-die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
-else
-{
-  echo "Connected succcessfully !! ";
-}
+if(mysqli_connect_errno()){
+die("Failed to connect to MySQL: " . mysqli_connect_error());}
+else{/*echo "Connected succcessfully !! ";*/}
 ?>
