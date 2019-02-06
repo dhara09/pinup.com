52c6ee4c27cfc7075e09a0f268950f4ae7bf80e5
dhara09 <dhara.salot@techsevin.com>
2019-02-06 14:29:48 +0530

    using session to display name

--- loginCheck.php
+++ loginCheck.php
@@ -1,52 +1,41 @@
 <?php
 if(!isset($_SESSION)){ 
     session_start();}
-/* $data=array(
-    $email=$_POST['Users']['email'],
-    $password=$_POST['Users']['password'],
-);   
-$query=http_build_query($data); */
+$errmsg='';
+$check=0;
+$email=$_POST['Users']['email'];
+$password=$_POST['Users']['password'];
 if(isset($_POST['Users']['email'])) 
 { 
-    $email=$_POST['Users']['email'];
-    $password=$_POST['Users']['password'];
-    $errmsg='';
-    $check=0;
-        if(empty($email))
-        {
+        if(empty($email)){
             $errmsg .= "Fill email </br>" ;
             $check=1;
         }
-        else if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-        {
+        else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
             $errmsg .= "Enter valid Email Address <br>" ;
             $check=1;
         }
-        if(empty($password))
-        {
+        if(empty($password)){
             $errmsg .= "Fill password" ;
             $check=1;
         }
-        if($check == 1)
-        {   
-            header("Location:http://local.pinup.com/login.php?error=$errmsg");
-           // $url="http://local.pinup.com/login.php?que=$query";
-            //$encode=rawurlencode($url);
-            //header("Location:http://local.pinup.co/login.php?.$encode");
-            //header("Location:http://local.pinup.com/login.php?error=".urlencode(serialize($query)));
-            //header("Location:http://local.pinup.com/login.php?error=$errmsg&em=$email&pass=$password");
-        }
-}
+        if($check == 0){   
+            header("Location:http://local.pinup.com/login.php?error=$errmsg&em=$email&pass=$password");
+        }   
+    }
+    //exit;
 require_once("connection.php");
 $email=$_POST['Users']['email'];
 $password=$_POST['Users']['password'];
-$pass=MD5($password);
-echo $query="SELECT * FROM User WHERE email='$email' and password='$pass'";
+//$pass=MD5($password);
+echo $query="SELECT * FROM User WHERE email='$email' and password='$password'";
 $result=mysqli_query($con,$query) or die(mysqli_error());
 echo $rows=mysqli_num_rows($result);
 if(!$rows = 1){
     echo "combination of name & pass dont match.<p>Click here to <a href='login.php'>Login</a></div></p>";
 }
 else{
-    header("Location: welcome.php");
+    session_start();
+		$_SESSION['email']=$email;
+		header("location: welcome.php");
 }  
