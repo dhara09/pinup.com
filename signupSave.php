7db945188fea5668e1223cbc272360e3b79b9557
dhara09 <dhara.salot@techsevin.com>
2019-02-07 19:10:41 +0530

    insert query modifications

--- signupSave.php
+++ signupSave.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+//echo "<pre>"; print_r($_REQUEST);//exit;
+/* $userArr=$_REQUEST[''];
+$userDetArr=$_REQUEST['']; */
 $data=array(
     $name =  $_POST['Users']['name'],
     $lname = $_POST['Users']['lastname'],
@@ -10,23 +14,21 @@ $data=array(
 );
 //echo $query=http_build_query($data);
 //$encode=rawurlencode($query);
-//header("location:signup.php?$encode");
-//header("location:signup.php?$query");
+//header("location:signup.php?data=".base64_encode(json_encode('$_REQUEST')));
+//header("location:signup.php?data=".rawurlencode($userArr));
 //exit;
-//$encode=rawurlencode($query);
-//header("location:signup.php?$encode"); exit;
 $errmsg='';
 $check=1;
 if(isset($_POST['Users']['name']) )
 { 
   
     if(empty($name)){   
-		$errmsg .="Please fill the Name <br>";
+		$errmsg .="Please Fill Your Name <br>";
         $check=0;
     } 
 
     else if(!preg_match("/^[a-zA-Z ]*$/",$name)) {
-        $errmsg .=" Use only Alphabets for name <br>";
+        $errmsg .=" Use only Alphabets for Name <br>";
         $check=0;
     }
     else if(strlen($name)<= 2){
@@ -35,91 +37,75 @@ if(isset($_POST['Users']['name']) )
     }
 
     if(empty($lname)){
-        $errmsg .="Please fill Your Last Name <br>";
+        $errmsg .="Please Fill Your Last Name <br>";
         $check=0;
     }
     else if(!preg_match("/^[a-zA-Z ]*$/",$lname)) {
-        $errmsg .= "Use only Alphabets for name <br>";
+        $errmsg .= "Use Only Alphabets for Name <br>";
         $check=0;
     }
     else if(strlen($lname)<=2){
-        $errmsg .="Enter valid LastName <br>";
+        $errmsg .="Enter Valid LastName <br>";
         $check=0;
     }
 
     if(empty($email)){
-        $errmsg .="Please fill your Email Address<br>";
+        $errmsg .="Please Fill your Email Address<br>";
         $check=0;
     }
     else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $errmsg .=" Enter valid Email Address <br>";
+        $errmsg .=" Enter Valid Email Address <br>";
         $check=0;
     }
 
     if(empty($address)){
-        $errmsg .="Please fill Your Address <br>";
+        $errmsg .="Please Fill Your Address <br>";
         $check=0;
     }
 
     if(empty($contact)){
-        $errmsg .="Please fill the Your Contact <br>";
+        $errmsg .="Please Fill the Your Contact <br>";
         $check=0;
     }
 
     else if(strlen($contact)>10){
-        $errmsg .= "Enter only 10 digits number <br>";
+        $errmsg .= "Enter 10 Digits Number <br>";
         $check=0;
     }
 
     if(empty($pass)){
-        $errmsg .="Please fill your Password <br>";
+        $errmsg .="Please Fill Your Password <br>";
         $check=0;
     }
     else if(strlen($pass)<=5){
-        $errmsg .="Enter mininmum 5-8 Characters <br>";
+        $errmsg .="Enter Mininmum 5-8 Characters <br>";
         $check=0;
     }        
     
     if(empty($cpass)){
-        $errmsg .=" Please confirm your password <br>";
+        $errmsg .=" Please Confirm Your password <br>";
         $check=0;
     }
 
     else if(strlen($cpass)<=5){
-        $errmsg .= "Enter minimum 5-8 characters <br>";
+        $errmsg .= "Enter Minimum 5-8 Characters <br>";
         $check=0;
 	}
 	
 	if($pass !== $cpass){
-		$errmsg .="passwords dnt match";
+		$errmsg .="Your Passwords Don't Match !!";
         $check=0;
     }
     if($check == 0){  
         header("Location:http://local.pinup.com/signup.php?error=$errmsg");
-        header("Location:http://local.pinup.com/signup.php?error=$errmsg&na=$name&ln=$lname&em=$email&ad=$address&con=$contact&pas=$pass&cp=$cpass");
         exit;
     }
 }
-//header("location:signup.php?$query");
-/* $data=array(
-    $name =>  $_POST['Users']['name'],
-    $lname => $_POST['Users']['lastname'],
-    $email => $_POST['Users']['email'],
-    $address => $_POST['UserDetail']['address'],
-    $contact => $_POST['UserDetail']['contact'],
-    $pass => $_POST['Users']['password'],
-    $cpass => $_POST['Users']['confirmpass'],
-);
-$query=http_build_query($data);
-$encode=rawurlencode($query);
-header("Location:sign.php?$encode"); */
 require_once("connection.php");
-$sql = "INSERT INTO User (name,lastname,email,password,confirmpass)
-VALUES (' ".$_POST['Users']['name']." ',' ".$_POST['Users']['lastname']." ',' ".$_POST['Users']['email']." ',' ".md5($_POST['Users']['password'])." ',' ".md5($_POST['Users']['confirmpass'])."')";
-if(!mysqli_query($con,$sql)){
+$query="INSERT INTO User(name,lastname,email,password) VALUES ('".$_POST['Users']['name']."','".$_POST['Users']['lastname']."','".$_POST['Users']['email']."','".md5($_POST['Users']['password'])."');";
+$query .="INSERT INTO userDetail(address,contact) VALUES('".$_POST['UserDetail']['address']."','".$_POST['UserDetail']['contact']."');";
+if (!mysqli_multi_query($con,$query)){
     die("<br> Error: Record not inserted ".mysqli_error());
 }
-else{
-    echo "<center></h3>To continue<br>Get urself logged</br></h3><h2><a href='login.php'>Login</a></h2></center>";
-}   
+else{  header("Location:login.php"); }
 ?> 
