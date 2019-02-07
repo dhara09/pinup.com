e8a769bffa8fadf01546a1d40d759b086dc513d8
dhara09 <dhara.salot@techsevin.com>
2019-02-07 19:03:09 +0530

    modification

--- ajax.php
+++ ajax.php
@@ -3,7 +3,7 @@ require_once("DBController.php");
 $db_handle = new DBController();
 if(!empty($_POST['email'])) 
 { 
-  $query = "SELECT email FROM User WHERE email='" . $_POST['email'] . "'";
+  $query = "SELECT email FROM User WHERE email='".$_POST['email']."'";
   $user_count = $db_handle->numRows($query);
   if($user_count > 0){
     echo "<span class='status-not-available'> Email Not Available.</span>";}
