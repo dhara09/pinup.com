76224ade250a7b43b5893c14c3e18682ecb1809e
dhara09 <dhara.salot@techsevin.com>
2019-02-08 18:04:31 +0530

    session

--- ajax.php
+++ ajax.php
@@ -3,11 +3,13 @@ require_once("DBController.php");
 $db_handle = new DBController();
 if(!empty($_POST['email'])) 
 { 
-  $query = "SELECT email FROM User WHERE email='".$_POST['email']."'";
+  $query = "SELECT * FROM User WHERE email='".$_POST['email']."'";
   $user_count = $db_handle->numRows($query);
   if($user_count > 0){
+    echo $user_count;
+    exit;
     echo "<span class='status-not-available'> Email Not Available.</span>";}
   else{
-    echo "<span class='status-available'> Email Available.</span>";}
+    echo "<span class='status-available'>Email Available.</span>";}
 }
 ?>
