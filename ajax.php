cc57ac11613733f3fcdd3b7c458bad69fbc24033
dhara09 <dhara.salot@techsevin.com>
2019-02-11 15:53:02 +0530

    specified the path

--- ajax.php
+++ ajax.php
@@ -1,13 +1,11 @@
 <?php
-require_once("DBController.php");
+require_once("./connection/DBController.php");
 $db_handle = new DBController();
 if(!empty($_POST['email'])) 
 { 
   $query = "SELECT * FROM User WHERE email='".$_POST['email']."'";
   $user_count = $db_handle->numRows($query);
   if($user_count > 0){
-    echo $user_count;
-    exit;
     echo "<span class='status-not-available'> Email Not Available.</span>";}
   else{
     echo "<span class='status-available'>Email Available.</span>";}
