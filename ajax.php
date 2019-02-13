ad30717ed60f018c0c473b89a178b522589e1219
dhara09 <dhara.salot@techsevin.com>
2019-02-13 19:04:04 +0530

    if else conditioning

--- ajax.php
+++ ajax.php
@@ -1,13 +1,15 @@
 <?php
+
 require_once("./connection/DBController.php");
 $db_handle = new DBController();
-if(!empty($_POST['email'])) 
-{ 
+if(!empty($_POST['email'])) { 
   $query = "SELECT * FROM User WHERE email='".$_POST['email']."'";
   $user_count = $db_handle->numRows($query);
+  /* if($user_count< 1){  echo "<span class='status-available'>Email Available</span>";  }
+    else{  echo "<span class='status-not-available'>Email Not Available</span>"; } */
   if($user_count > 0){
-    echo "<span class='status-not-available'> Email Not Available.</span>";}
+    echo "<span class='status-not-available'>Email Not Available</span>";}
   else{
-    echo "<span class='status-available'>Email Available.</span>";}
+    echo "<span class='status-available'>Email Available</span>";} 
 }
 ?>
