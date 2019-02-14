eaf316676bc95c02d8467932364704eb589e7aa5
dhara09 <dhara.salot@techsevin.com>
2019-02-14 19:25:39 +0530

    validations

--- signupSave.php
+++ signupSave.php
@@ -1,20 +1,12 @@
 <?php session_start();
-echo "<pre>"; print_r($_REQUEST); echo "</pre>";
-$data=array(
-    $name =  $_POST['Users']['name'],
-    $lname = $_POST['Users']['lastname'],
-    $email = $_POST['Users']['email'],
-    $address = $_POST['UserDetail']['address'],
-    $contact = $_POST['UserDetail']['contact'],
-    $pass = $_POST['Users']['password'],
-    $cpass = $_POST['Users']['confirmpass'],
-    $cap=$_POST['Users']['cap']
-);
-//echo $query = http_build_query(array('url' => $data));
-//echo $query1=base64_encode(json_encode(http_build_query(array('login' => $data))));
-echo $query1=base64_encode(json_encode(http_build_query($data)));
-exit;
-//header("location:signup.php?data=".base64_encode(json_encode('$_REQUEST')));
+    $name =  $_POST['Users']['name'];
+    $lname = $_POST['Users']['lastname'];
+    $email = $_POST['Users']['email'];
+    $address = $_POST['UserDetail']['address'];
+    $contact = $_POST['UserDetail']['contact'];
+    $pass = $_POST['Users']['password'];
+    $cpass = $_POST['Users']['confirmpass'];
+    $cap=$_POST['Users']['cap'];
 $errmsg='';
 $check=1;
 if(isset($_POST['Users']['name']) )
@@ -66,7 +58,7 @@ if(isset($_POST['Users']['name']) )
         $check=0;
     }
 
-    else if(strlen($contact)>10){
+    else if(strlen($contact) < 10){
         $errmsg .= "Enter 10 Digits Number <br>";
         $check=0;
     }
@@ -76,7 +68,7 @@ if(isset($_POST['Users']['name']) )
         $check=0;
     }
     else if(strlen($pass)<=5){
-        $errmsg .="Enter Mininmum 5-8 Characters <br>";
+        $errmsg .="Enter Mininmum 5-8 Characters For Password <br>";
         $check=0;
     }        
     
@@ -86,21 +78,23 @@ if(isset($_POST['Users']['name']) )
     }
 
     else if(strlen($cpass)<=5){
-        $errmsg .= "Enter Minimum 5-8 Characters <br>";
+        $errmsg .= "Enter Minimum 5-8 Characters For Confirm Password<br>";
         $check=0;
 	}
 	
 	if($pass !== $cpass){
-		$errmsg .="Your Passwords Don't Match !!";
+		$errmsg .="And So Your Passwords Don't Match !!";
         $check=0;
     }
-    if(empty($cap)){
-        $errmsg .= "Enter the text you see!!";
+    /* if(empty($cap)){
+        $errmsg .="Please Fill Captcha text ";
         $check=0;
-    }
-    if($check == 0){  
-        header("Location:signup.php?error=$errmsg");
-        exit;
+    } */
+    if($check == 0){ 
+       // $err=base64_encode(json_encode(($errmsg))); 
+     header("location:signup.php?error=$errmsg&na=$name&ln=$lname&email=$email&ad=$address&cn=$contact&ps=$pass&cp=$cpass");
+       //header("Location:signup.php?error=$err");
+       exit;
     }
 }
 require_once("./connection/connection.php");
@@ -108,13 +102,13 @@ $date = date('Y-m-d');
 $email=$_POST['Users']['email'];
 $duplicate=mysqli_query($con,"select email from User where email='$email'");
 if (mysqli_num_rows($duplicate)>0){
-    $errmsg .= "Email You Entered Exists already..";
+   $errmsg .= "Email You Entered Exists already..";
     header("Location:signup.php?error=$errmsg"); }
 else{
         $query ="INSERT INTO User(name,lastname,email,password,createdDate,createdTime) VALUES ('".$_POST['Users']['name']."','".$_POST['Users']['lastname']."','".$_POST['Users']['email']."','".md5($_POST['Users']['password'])."','".$date."','".time('H:M:S')."');";
-        $query .="INSERT INTO userDetail(address,contact,createdDate,createdTime) VALUES('".$_POST['UserDetail']['address']."','".$_POST['UserDetail']['contact']."','".$date."','".time('h:m:s')."');";
+        $query .="INSERT INTO userDetail(address,contact,createdDate,createdTime) VALUES('".$_POST['UserDetail']['address']."','".$_POST['UserDetail']['contact']."','".$date."','".time('H:M:S')."');";
         if (!mysqli_multi_query($con,$query)){
             die("<br> Error: Record not inserted ".mysqli_error()); }
         else {  header("Location:login.php?status=success"); }
-    }      
+    }        
 ?> 
