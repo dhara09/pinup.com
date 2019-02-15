a2d6dc6115b5387ca833e48cf4eeb847475a1334
dhara09 <dhara.salot@techsevin.com>
2019-02-15 19:42:52 +0530

    ajax conditions

--- ajax.php
+++ ajax.php
@@ -9,9 +9,15 @@ if(!empty($_POST['email']))
   if($user_count > 0)
   {
     echo "<span class='status-not-available'>Email Not Available</span>";
+    
+    /* $duplicate=mysqli_query($con,"Select email from User where email='$email'");
+    if (mysqli_num_rows($duplicate) >0){
+       echo "<span class='status-not-available'> Email You Entered Exists already.'";
+       return false;
+    }  */
   }
   else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    echo "<span class='status-available'>Enter Valid Email </span>";
+    echo "<span class='status-not-available'>Enter Valid Email </span>";
   }
   else{
     echo "<span class='status-available'>Email Available </span>";
