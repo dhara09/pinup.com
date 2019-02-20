88d6d706841aa2052b53f718a8b5c5006fb3e89a
dhara09 <dhara.salot@techsevin.com>
2019-02-20 17:00:13 +0530

    sessions

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
+<?php session_start (); ?>
 <html>
 <center>
 <button onclick="window.location.href='login.php'" type="submit" value="submit" name="submit">Login</button>
 <button onclick="window.location.href='signup.php'" type="submit" value="submit" name="submit">Signup</button>
 </center>
-<?php
-include('footer.php');
-?>
+<?php require_once('footer.php'); ?>
