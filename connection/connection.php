5014b537b27452e6f5d0179f8a074d6091557ae0
dhara09 <dhara.salot@techsevin.com>
2019-02-21 17:34:18 +0530

    global variable

--- connection/connection.php
+++ connection/connection.php
@@ -3,6 +3,7 @@ $host="localhost";
 $user="root";
 $password="123456";
 $dbname="userDatabase";
+global $con;
 $con=mysqli_connect($host,$user,$password,$dbname);
 if(mysqli_connect_errno()){
 die("Failed to connect to MySQL: " . mysqli_connect_error());}
