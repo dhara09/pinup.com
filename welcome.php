23be2ad4f044aa83590cdda83f652eb264010b8c
dhara09 <dhara.salot@techsevin.com>
2019-02-21 17:35:46 +0530

    using sessions

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
 include("auth.php");
 session_start();
 if(!isset($_SESSION['email'])){
- header("location: welcome.php");}
+header("location: welcome.php");}
 $email=$_SESSION['email'];
 ?>
 <!DOCTYPE html>
