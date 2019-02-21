feae6a6ef4a6d0a7b1376b53a9e525f7b2414695
dhara09 <dhara.salot@techsevin.com>
2019-02-21 17:38:42 +0530

    using sessions

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
-<?php session_start();
-//include_once("../css/stylesheet.css"); ?>
+<?php 
+$email=$_POST['email'];
+session_start();
+if(isset($_SESSION['email'])){
+ header("Location:welcome.php");} 
+?>
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login Page</title>
