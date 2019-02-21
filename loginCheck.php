c306737f10b1f271f0fa2b7107d3388b7b0f5eca
dhara09 <dhara.salot@techsevin.com>
2019-02-21 18:49:29 +0530

    to check for email and md5 password

--- loginCheck.php
+++ loginCheck.php
@@ -27,8 +27,9 @@ require_once("./connection/connection.php");
 $email=$_POST['Users']['email'];
 $password=$_POST['Users']['password'];
 $url1=base64_encode($email);
-//$userpass=md5($password);
-$query="SELECT * FROM User WHERE email='$email' and password='$password'";
+$userpass=md5($password);
+$query="SELECT * FROM User WHERE email='$email' and password='$userpass'";
+global $con;
 $result=mysqli_query($con,$query) or die(mysqli_error());
 $rows=mysqli_num_rows($result);
 if($rows == 1){  
