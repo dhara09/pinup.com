5716f0d63db7658ffe1fa7361aab251cdcf84e3e
dhara09 <dhara.salot@techsevin.com>
2019-02-22 13:29:04 +0530

    sessions changes

--- logout.php
+++ logout.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-if(isset($_SESSION['email'])) {
-    unset($_SESSION['email']); }
+if(isset($_SESSION['email'])) 
+{
+    unset($_SESSION['email']); 
+}
 header("Location:login.php");
-
 ?>
