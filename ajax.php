c3f575a2cf799c4cd5759bb165aeb004c2f719dc
dhara09 <dhara.salot@techsevin.com>
2019-02-22 13:27:52 +0530

    table name changed

--- ajax.php
+++ ajax.php
@@ -4,18 +4,10 @@ $db_handle = new DBController();
 $email=$_POST['email'];
 if(!empty($_POST['email'])) 
 { 
-  $query = "SELECT * FROM User WHERE email='".$_POST['email']."'";
+  $query = "SELECT * FROM user WHERE email='".$_POST['email']."'";
   $user_count = $db_handle->numRows($query);
-  if($user_count > 0)
-  {
-    echo "<span class='status-not-available'>Email Not Available</span>";
-    
-    /* $duplicate=mysqli_query($con,"Select email from User where email='$email'");
-    if (mysqli_num_rows($duplicate) >0){
-       echo "<span class='status-not-available'> Email You Entered Exists already.'";
-       return false;
-    }  */
-  }
+  if($user_count > 0){
+    echo "<span class='status-not-available'>Email Not Available</span>"; }
   else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
     echo "<span class='status-not-available'>Enter Valid Email </span>";
   }
